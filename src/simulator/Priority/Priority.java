8c6e140f42d40d7a001b75f8a21154d791eba09e
Rawan Tarek <rawan25112003@gmail.com>
2023-12-15 11:26:40 +0200

    Optimizing priority algorithm

--- src/simulator/Priority/Priority.java
+++ src/simulator/Priority/Priority.java
@@ -10,16 +10,14 @@ import java.util.List;
 public class Priority {
 
     public ArrayList<ProcessDuration> priorityScheduling(ArrayList<Process> processes) {
-        double totalWaitingTime = 0, totalTurnAroundTime = 0, AgingInterval = 5;
+        double AgingInterval = 5;
         List<ProcessDuration> sortedProcesses = new ArrayList<>();
 
-        // Convert processes to ProcessDuration and add to sortedProcesses
         for (Process process : processes) {
             sortedProcesses.add(new ProcessDuration(process.getName(), process.getArrivalTime(),
                     process.getBurstTime(), process.getPriority(), process.getColor()));
         }
 
-        // Sort processes based on arrival time and priority
         sortedProcesses.sort(Comparator.comparingInt(ProcessDuration::getArrivalTime)
                 .thenComparingInt(ProcessDuration::getPriority));
 
@@ -28,25 +26,18 @@ public class Priority {
                     sortedProcesses.get(sortedProcesses.indexOf(process) - 1).getEndTime());
             process.setEndTime(process.getStartTime() + process.getBurstTime());
 
-            // Calculate waiting time and update process attributes
             int waitingTime = Math.max(0, process.getStartTime() - process.getArrivalTime());
             process.setWaitingTime(waitingTime);
 
-            // Aging logic
             if (waitingTime >= AgingInterval && !process.getProcessIsDone()) {
                 process.increasePriority();
             }
 
-            // Calculate turnaround time and update attributes
             int turnaroundTime = process.getEndTime() - process.getArrivalTime();
             process.setTurnAroundTime(turnaroundTime);
 
-            // Update process status as done after scheduling
             process.setProcessIsDone(true);
 
-            // Accumulate waiting and turnaround times
-            totalWaitingTime += waitingTime;
-            totalTurnAroundTime += turnaroundTime;
         }
 
         return new ArrayList<>(sortedProcesses);
